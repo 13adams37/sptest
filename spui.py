b7eebe43739d7a6c8370a5b076c9daea02393684
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-09-12 11:22:25 +0300

    Done add pages (ui). WIP

--- spui.py
+++ spui.py
@@ -3,20 +3,88 @@
 import PySimpleGUI as sg
 
 
+class Pages:
+    def __init__(self):
+        self.addtswindow = None
+        self.addwindow = None
+
+    def addaddpage(self):
+        addpage = [
+            [sg.Text('Добавить организацию', key="-AddKomers-", enable_events=True, justification="center",
+                     expand_x=True,
+                     expand_y=True)],
+            [sg.Text('Добавить проверенное ТС', key="-AddTs-", enable_events=True, justification="center",
+                     expand_x=True,
+                     expand_y=True)],
+            [sg.Text('Закрыть', key="-CloseAddPage-", enable_events=True, justification="center",
+                     expand_x=True)]
+        ]
+        self.addwindow = sg.Window('AddPage', addpage, resizable=True).Finalize()
+
+    def addtspage(self):
+        addtspage = [
+            [sg.Text('Договор'), sg.InputText(key='-DOGOVOR-')],
+            [sg.Text('Акт'), sg.InputText(key='-AKT-')],
+            [sg.Text('Наименование ТС'), sg.InputText(key='-TSNAME-')],
+            [sg.Text('Модель'), sg.InputText(key='-MODEL-')],
+            [sg.Text('Заводской номер'), sg.InputText(key='-PARTNUMBER-')],
+            [sg.Text('Производитель'), sg.InputText(key='-VENDOR-')],
+            [sg.Text('СЗЗ-1'), sg.InputText(key='-CZZ1-')],
+            [sg.Text('СЗЗ-2'), sg.InputText(key='-CZZ2-')],
+            [sg.Text('УФ'), sg.InputText(key='-UF-')],
+            [sg.Text('РГГ'), sg.InputText(key='-RGG-')],
+            [sg.Text('РГГ пп'), sg.InputText(key='-RGGPP-')],
+            [sg.Text('Признак (уровень)'), sg.InputText(key='-LEVEL-')],
+            [sg.Text('Степень секретности'), sg.InputText(key='-SS-')],
+            [sg.Text('Категорий помещения'), sg.InputText(key='-KP-')],
+            [sg.Text('Закрыть', key="-CloseAddTsPage-", enable_events=True, justification="center",
+                     expand_x=True)]
+        ]
+        self.addtswindow = sg.Window('AddPage', addtspage, resizable=True,
+                                     element_justification="right").Finalize()
+
+
 class SpUi:
-    mainpage = [
-        [sg.Text('test0')],
-        [sg.Text('org name'), sg.InputText()],
-        [sg.Submit('Дальше', size=(10, 0), button_color='green'), sg.Cancel('Выход', button_color='red')]
-    ]
 
     def makeui(self):
+        pages = Pages()
         sg.theme('DarkAmber')
-        window = sg.Window('Window Title', self.mainpage)
-        # Event Loop to process "events" and get the "values" of the inputs
-        while True:
+
+        mainpage = [
+            [sg.Text('Добавление', key="-Add-", enable_events=True, justification="center", expand_x=True,
+                     expand_y=True)],
+            [sg.Text('Редактирование', key="-Edit-", enable_events=True, justification="center", expand_x=True,
+                     expand_y=True)]
+        ]
+        window = sg.Window('MainPage', mainpage, resizable=True).Finalize()
+
+        while True:  # MainPage
             event, values = window.read()
-            if event == sg.WIN_CLOSED or event == 'Выход':  # if user closes window or clicks cancel
+
+            if event == "-Add-":
+                window.Hide()
+                pages.addaddpage()
+
+                while True:  # Add Page
+                    event, values = pages.addwindow.read()
+
+                    if event == "-AddTs-":
+                        pages.addwindow.Hide()
+                        pages.addtspage()
+                        while True:  # TSPage
+                            event, values = pages.addtswindow.read()
+                            # Реализация окна добавления ТС
+                            if event == sg.WIN_CLOSED or event == "-CloseAddTsPage-":
+                                pages.addwindow.UnHide()
+                                pages.addtswindow.close()
+                                break
+
+                    if event == sg.WIN_CLOSED or event == "-CloseAddPage-":
+                        window.UnHide()
+                        pages.addwindow.close()
+                        break
+
+            if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
                 break
-            print('You entered ', values[0])
+            # event, values
         window.close()
