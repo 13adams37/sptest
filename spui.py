5930f9fd609176fd380289a57ee686aace8e17c1
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-09-21 11:01:37 +0300

    updated table list methods. ver 0.0.2

--- spui.py
+++ spui.py
@@ -3,6 +3,7 @@
 import PySimpleGUI as sg
 
 NULLLIST = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+table1, table2 = [], []
 
 
 class Pages:
@@ -14,7 +15,8 @@ class Pages:
 
         self.dogovornumber = None
         self.actnumber = None
-        self.tsdata = None
+        self.tsdata = []
+        # self.tabledata = None
         self.tsavailable = ["Изделие", "Элемент", "Составная часть"]
 
     def mainpage(self):
@@ -97,10 +99,10 @@ class Pages:
                 self.credentialswindow.close()
                 return 0
 
-    def addtspage(self):
+    def addtspage(self, master, headername):
         headings = ['Дог.', 'Акт', 'Наим.', 'Модель', 'S/N', 'Произв.', 'С1', 'С2', 'УФ', 'РГГ', 'РГГ пп', 'П', 'Сек',
                     'Кат.', 'Состав']
-        self.tsdata = [NULLLIST, ]
+        tabledata = [NULLLIST, ]
         addtspage = [
             [sg.Column(
                 [[sg.Text('Договор'),
@@ -116,14 +118,14 @@ class Pages:
                  # input + spisok. link model, vendor
                  [sg.Text('Модель'),
                   sg.InputCombo(["get from bd + kostil"], key='model', size=(45, 0)),
-                  sg.Checkbox("Save", k="-MODELSAVE-")],
+                  sg.Checkbox("Сохр.", k="-MODELSAVE-")],
                  # input, link tsname, vendor
                  [sg.Text('Заводской номер'), sg.InputText(key='partnumber', enable_events=True),
                   sg.Checkbox("б/н", k="nopart", enable_events=True)],  # mb add save button
                  [sg.Text('Производитель'), sg.InputCombo(["get from bd + kostil"], key='vendor', size=(45, 0)),
-                  sg.Checkbox("Save", k="-VENDORSAVE-")],
+                  sg.Checkbox("Сохр.", k="-VENDORSAVE-")],
                  # input + spisok. link tsname, model
-                 [sg.Text('СЗЗ-1'), sg.InputText(key='serial1'), sg.Checkbox("Авто", k="-CZZAUTO-")],
+                 [sg.Text('СЗЗ-1'), sg.InputText(key='serial1'), sg.Checkbox("Сохр.", k="-CZZAUTO-")],
                  # locked, checkbox + nextint
                  [sg.Text('СЗЗ-2'), sg.InputText(key='serial2')]]
                 , justification="r", element_justification="r"
@@ -142,17 +144,18 @@ class Pages:
                   sg.Text('Категория помещения'), sg.Combo(["1", "2"], readonly=True, key='kp', s=(5, 0))]],
                 justification="c", element_justification="c"
             )],
-            [sg.Table(self.tsdata, headings=headings, justification='l', key="-TABLE-", visible=False,
-                      auto_size_columns=False, max_col_width=5)],
+            [sg.Table(tabledata, headings=headings, justification='l', key="-TABLE-", visible=False,
+                      auto_size_columns=True, max_col_width=5)],
             [sg.Text('Закрыть', key="-CloseAddTsPage-", enable_events=True, justification="left", expand_x=True),
              sg.Button("Сохранить"),
              sg.Submit('Обновить', size=(10, 0), k="-REFR-", button_color='gray', p=(20, 0)),
              sg.Button("Новое ТС"),
              ]
         ]
-        self.addtswindow = sg.Window('AddTsPage', addtspage, resizable=True, element_justification="").Finalize()
+        self.addtswindow = sg.Window(headername, addtspage, resizable=True,
+                                     element_justification="").Finalize()
         table = self.addtswindow['-TABLE-']
-        self.tsdata = []
+        tabledata.clear()
         if self.tsavailable == ["Составная часть"]:
             self.addtswindow['level'].update()
         if "Изделие" in self.tsavailable or "Элемент" in self.tsavailable:
@@ -162,15 +165,11 @@ class Pages:
             self.addtswindow["-ADDMORE-"].update(visible=False)
             self.addtswindow["-TABLE-"].update(visible=False)
 
-        # state handler:
-        # 0 = master, can add two slaves, or not, depends of TS type!
-        # 1 = slave1 of master, can add slave2 or no, depends on type of TS!
-        # 2 = slave2 of slave1, slave2 cant add more slaves
-
         while True:  # TSPage
             event, values = self.addtswindow.read()
-            # TSPage realisation
+
             if event == "-REFR-":
+                print(values["-TABLE-"])  # get a picked table element position
                 print(table.Get())
             elif event == "level":
                 if values[event] == "Изделие" or values[event] == "Элемент":
@@ -187,24 +186,28 @@ class Pages:
                     page2.tsavailable = ["Элемент", "Составная часть"]
                     page2.actnumber = self.actnumber
                     page2.dogovornumber = self.dogovornumber
+                    page2.tsdata = page3.tsdata
 
-                    page2.addtspage()
-
-                    if NULLLIST not in page2.tsdata and page2.tsdata:
-                        self.tsdata.append(page2.tsdata)
-                        table.Update(self.tsdata)
+                    page2.addtspage(master=False, headername="Добавление 2-го уровня")
+                    table.Update(table1)
+                    # if not master:
+                    #     table1.clear()
+                    #     print("deleting table1")
+                    print("updating table1")
                 else:
                     page3.tsavailable = ["Составная часть"]
                     page3.actnumber = self.actnumber
                     page3.dogovornumber = self.dogovornumber
 
-                    page3.addtspage()
-                    if NULLLIST not in page3.tsdata and page3.tsdata:
-                        self.tsdata.append(page3.tsdata)
-                        table.Update(self.tsdata)
+                    page3.addtspage(master=False, headername="Добавление 3-го уровня")
+                    table.Update(table2)  # insert tsvalues into table2
+                    # if not master:
+                    #     table2.clear()
+                    #     print("deleting table2")
+                    print("updating table2")
 
                 print("debug master")
-                print(self.tsdata)
+
             elif event == "nopart":
                 if values[event]:
                     self.addtswindow["partnumber"].update("б/н", disabled=True)
@@ -212,23 +215,58 @@ class Pages:
                     self.addtswindow["partnumber"].update("", disabled=False)
 
             elif event == "Сохранить":
-                self.tsdata = self.get_ts_values(values)
+                # ts_values = self.get_ts_values(values)
+                # self.tsdata.append(ts_values)  # extend or append?
+                # if self.tsdata != ts_values:
+                #     # self.tsdata += ts_values
+                #     self.tsdata.extend(ts_values)
+                # else:
+                #     print("you shall not pass")
+
+                # table1.extend(self.tsdata) if values["level"] == "Изделие" else table2.append(self.tsdata)
+
+                # push to db if master
+
+                if values["level"] == "Элемент":
+                    # put tsvalues (L2) into table1
+                    self.insert_values_into_table(self.get_tsvalues(values), table1)
+                else:
+                    # put tsvalues (L3) into table2
+                    self.insert_values_into_table(self.get_tsvalues(values), table2)
+
+            elif event == "Новое ТС":
+                # clear fileds without checkboxes
+                print("new ts")
 
             elif event == sg.WIN_CLOSED or event == "-CloseAddTsPage-":
+                if values["level"] == "Изделие" and not master:
+                    print("deleting table1")
+                    table1.clear()
+                elif values["level"] == "Элемент" and not master:
+                    print("deleting table2")
+                    table2.clear()
                 self.addtswindow.close()
-                return 0
+                break
 
-    def get_ts_values(self, values):
+    def insert_values_into_table(self, values, table):
+        table.append(values)
+
+    def get_tsvalues(self, values):
         allowed_list = ["dogovor", "act", "name", "model", "partnumber", "vendor", "serial1", "serial2", "uv", "rgg",
                         "rggpp", "level", "ss", "kp"]
         listed = []
-        req = self.addtswindow["-TABLE-"].Get()
+
         for value in values:
             if value in allowed_list:
                 listed.append(values[value])
-        if NULLLIST not in req and req:
-            listed.append(req)
-        print(listed)
+        if values["level"] == "Элемент" or "Изделие":
+            temptable = []
+            tables = self.addtswindow["-TABLE-"].Get()
+            for table in tables:
+                temptable.append(table)
+            print("temp table =", temptable)
+            listed.append(temptable)
+        print("listed =", listed)
         return listed
 
 
@@ -246,12 +284,12 @@ class SpUi:
                 pages.addaddpage()
                 while True:  # Add Page
                     event, values = pages.addwindow.read()
-                    # AddPage realisation
-                    if event == "-AddTs-":
+                    if event == "-AddTs-":  # AddPage
                         pages.addwindow.Hide()
-                        if pages.credentialspage:  # if closed check + state check!!!!!
-                            pages.tsstate = 0
-                            pages.addtspage()
+                        if pages.credentialspage:  # close check
+                            pages.addtspage(master=True, headername="Добавление технического средства")
+                            table1.clear()
+                            table2.clear()
 
                         pages.addwindow.UnHide()
                     elif event == "-AddKomers-":
