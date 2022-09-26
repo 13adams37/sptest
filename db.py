570a01cb40a5e950e52627c56ed9196b51d55d8b
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-09-26 16:37:05 +0300

    fix makejson

--- db.py
+++ db.py
@@ -16,30 +16,24 @@ class DataBase:
     def get(self, what):
         pass
 
-    def makejson(self, elements):
-        tempdict = {}
-        tempdict1 = {}
-        tempdict2 = {}
-        listdict1 = {}
-        listdict2 = {}
+    def makejson(self, elements):  # making dict
+        tempdict, tempdict1, tempdict2 = {}, {}, {}
+        listdict1, listdict2 = [], []
         for key, element in zip(keys, elements):
             tempdict[key] = element
             if key == "table" and element:
-                v = 0
                 for element1 in element:
                     for key1, element2 in zip(keys, element1):
                         tempdict1[key1] = element2
                         if key1 == "table" and element2:
-                            z = 0
                             for element3 in element2:
                                 for key2, element4 in zip(keys, element3):
                                     tempdict2[key2] = element4
-                                listdict2.update({z: tempdict2.copy()})
-                                z = z + 1
+                                listdict2.append(tempdict2.copy())
                                 tempdict2.clear()
-                            tempdict1[key1] = listdict2
-                    listdict1.update({v: tempdict1.copy()})
-                    v = v + 1
+                            tempdict1[key1] = listdict2.copy()
+                            listdict2.clear()
+                    listdict1.append(tempdict1.copy())
                     tempdict1.clear()
                 tempdict[key] = listdict1.copy()
         return tempdict
@@ -47,3 +41,4 @@ class DataBase:
 
 if __name__ == '__main__':
     test = DataBase()
+    # print(db.all())
